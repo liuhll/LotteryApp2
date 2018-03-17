25ef57ea00e3eab23a05782368eebc42d23aedcc
liuhll <1029765111@qq.com>
2018-03-17 20:30:25 +0800

    新增积分商城功能模块

--- src/views/pointmall/index.vue
+++ src/views/pointmall/index.vue
@@ -1,3 +1,49 @@
 <template>
-  <div>积分商城</div>
+   <div class="point-mall-container">
+    <grid :cols='4'>  
+      <grid-item label="我的积分">
+       <img slot="icon">
+      </grid-item>   
+      <grid-item label="积分记录">
+       <img slot="icon">
+      </grid-item>
+      <grid-item label="积分购买">
+       <img slot="icon">
+      </grid-item>
+      <grid-item label="签到">
+       <img slot="icon">
+      </grid-item>
+      <grid-item label="分享App">
+       <img slot="icon">
+      </grid-item>     
+    </grid>
+  </div>
 </template>
+<script>
+import { Grid, GridItem } from 'vux'
+
+export default {
+  components: {
+    Grid,
+    GridItem
+  }
+}
+</script>
+
+<style lang="less">
+.app-main {
+  margin-bottom:10px;
+  width: 100%;
+}
+.weui-grid {
+  padding: 4px 4px !important;
+}
+.weui-grid__icon {
+  width: 16px!important;
+  height: 16px!important;
+}
+.weui-grid__label {
+  font-size: 12px!important;
+  color: #333333!important;
+}
+</style>
