685123697f068dd159d46fd7fd66c9330a0d5e44
liuhll <1029765111@qq.com>
2018-04-15 16:16:03 +0800

    新增分享功能菜单

--- src/views/pointmall/index.vue
+++ src/views/pointmall/index.vue
@@ -17,7 +17,7 @@
       <grid-item label="活动与公告" @on-item-click="onActivity">
        <img slot="icon" src="../../assets/images/pointmall/activity.svg">
       </grid-item>   
-      <grid-item label="分享App">
+      <grid-item label="分享App" @on-item-click="onShare">
        <img slot="icon" src="../../assets/images/pointmall/share.svg">
       </grid-item>     
     </grid>
@@ -25,10 +25,16 @@
     <activity v-if="topic.index===0"></activity>
     <points-info v-if="topic.index===1" :signedInfo="signedInfo"></points-info>
     <points-record v-if="topic.index===2" :pointsRecords="pointsRecords"></points-record>
+    <actionsheet v-model="shareApp.show" 
+      :menus="shareApp.menus" 
+      :close-on-clicking-mask="false" 
+      show-cancel 
+      @on-click-mask="console.log('on click mask')">
+      </actionsheet>
   </div>
 </template>
 <script>
-import { Grid, GridItem, Divider } from "vux";
+import { Grid, GridItem, Divider, Actionsheet } from "vux";
 import Activity from "./components/activity";
 import PointsInfo from "./components/points-info"
 import PointsRecord from "./components/points-record"
@@ -40,7 +46,8 @@ export default {
     Divider,
     Activity,
     PointsInfo,
-    PointsRecord
+    PointsRecord,
+    Actionsheet
   },
   data() {
     return {
@@ -55,7 +62,14 @@ export default {
       signedInfo: {
 
       },
-      pointsRecords: []
+      pointsRecords: [],
+      shareApp: {
+        show: false,
+        menus: {
+          menu1: '分享给好友',
+          menu2: '分享到朋友圈'
+        } 
+      }
     };
   },
   mounted() {
@@ -118,6 +132,9 @@ export default {
     getMyPointRecords() {
       this.topic.text = "积分记录"
       this.topic.index = 2
+    },
+    onShare() {
+      this.shareApp.show = true
     }
   }
 };
